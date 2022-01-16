f917feb5e65ab89d30f7a6839dc11ccb1e767d43
Paul T. Little <paul.little@akamesh.com>
2022-01-16 07:15:17 -0700

    upgrade to Java 11 and logback

--- src/main/java/org/jdamico/javax25/App.java
+++ src/main/java/org/jdamico/javax25/App.java
@@ -1,13 +1,17 @@
 package org.jdamico.javax25;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App 
 {
     public static void main( String[] args )
     {
+    	log.info("starting App");
         System.out.println( "Hello World!" );
     }
 }
